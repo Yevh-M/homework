695101b1357fa541b7ea874159dd39e80a32b584
Yevh-M <mynike37@gmail.com>
2017-08-01 14:03:34 +0300

    Created new folder for homework php_devionity, first array

---
+++ php_devionity/index1.php
@@ -0,0 +1,13 @@
+<?php
+// Створити масив з 5 країн,
+// вивести масив на екран за допомогою print_r з використанням тега форматованого виведення
+
+$countries = array(China, India, USA, Indonesia, Brazil);
+
+echo '<pre>';
+print_r($countries);
+echo '</pre>';
+?>
+
+
+
