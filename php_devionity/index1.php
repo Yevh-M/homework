fb32898348e7a4453df42e7031fe812177a95015
Yevh-M <mynike37@gmail.com>
2017-08-01 16:09:50 +0300

    A multidimensional array is added

--- php_devionity/index1.php
+++ php_devionity/index1.php
@@ -18,5 +18,19 @@ echo '<pre>';
 print_r($countries);
 echo '</pre>';
 
+?>
+
+<?php
+//Багатовимірний масив
+
+$book1 = array('style' => novel, 'author' => Ernest_Hemingway, 'name' => The_Old_Man_and_the_Sea, 'price' => 9.99,);
+$book2 = array('style' => psychological_horror, 'author' => Stephen_King, 'name' =>  Duma_Key, 'price' => 15.95,);
+$book3 = array('style' => quest, 'author' => Paulo_Coelho, 'name' => The_Alchemist, 'price' => 16.39,);
+$books_shop = array($book1, $book2, $book3);
+
+echo '<pre>';
+print_r ($books_shop);
+echo '</pre>';
+
 
 
