2fdbc99dc953235466e98b2ea9cf2b3478d79335
Yevh-M <mynike37@gmail.com>
2017-08-02 17:11:38 +0300

    Define constants

---
+++ php_devionity/define_constants.php
@@ -0,0 +1,13 @@
+<?php
+
+//Константи
+
+define ('CHINA', array('Beijing', 9596961, 'Yuan'));
+define ('INDIA', array('New Delhi', 3287263, 'Indian rupee'));
+define ('USA', array('Washington', 3796742, 'United States dollar'));
+
+echo CHINA[0];
+echo '<br/>';
+echo INDIA[0];
+echo '<br/>';
+echo USA[0];
