ac4c6674c454213909cecf09b82bfa66401896cd
Yevh-M <mynike37@gmail.com>
2017-08-04 13:39:21 +0300

    Task 16. Maximum value of the number

---
+++ php_basics_tasks/16.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = 5;
+$b = 50;
+
+if ($a > $b)
+{
+    echo "Number $a is greater than number $b";
+}
+elseif  ($a == $b)
+{
+    echo 'These numbers are equal';
+}
+else {
+    echo "Number $b is greater than number $a";
+}
