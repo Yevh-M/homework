273eba5a3dbd1fd670c11e9433132232630db840
Yevh-M <mynike37@gmail.com>
2017-08-04 12:58:32 +0300

    Tasks 9 to 12

---
+++ php_basics_tasks/10.php
@@ -0,0 +1,21 @@
+<?php
+
+$day = 3;
+
+switch ($day) {
+    case 1:
+        echo "This is a working day";
+        break;
+    case 2:
+        echo "This is a working day";
+        break;
+    case 3:
+        echo "This is a working day";
+        break;
+    case 4:
+        echo "This is a working day";
+        break;
+    case 5:
+        echo "This is a working day";
+        break;
+}
---
+++ php_basics_tasks/11.php
@@ -0,0 +1,27 @@
+<?php
+
+$day = 7;
+
+switch ($day) {
+    case 1:
+        echo "This is a working day";
+        break;
+    case 2:
+        echo "This is a working day";
+        break;
+    case 3:
+        echo "This is a working day";
+        break;
+    case 4:
+        echo "This is a working day";
+        break;
+    case 5:
+        echo "This is a working day";
+        break;
+    case 6:
+        echo "It's a day off";
+        break;
+    case 7:
+        echo "It's a day off";
+        break;
+}
