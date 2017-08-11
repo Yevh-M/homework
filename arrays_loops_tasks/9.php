30cbe0f0e7ea7094a923c06e49583a07045cbb2b
Yevh-M <mynike37@gmail.com>
2017-08-11 16:27:12 +0300

    Outputs numbers in a column using cycles <while> and <for>

---
+++ arrays_loops_tasks/9.php
@@ -0,0 +1,20 @@
+<?php
+
+//using cycle while
+
+$i = 1;
+
+while($i <= 100)
+{
+    echo "$i <br/>";
+    
+	$i++;
+}
+
+//using cycle for
+
+for($n = 1; $n <= 100; $n++)
+{
+    echo "$n <br/>";
+
+}
