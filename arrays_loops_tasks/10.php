88533d820303d4dff029562379c61273b43041fb
Yevh-M <mynike37@gmail.com>
2017-08-11 21:33:46 +0300

    Output span of values using loops <while> and <for>

---
+++ arrays_loops_tasks/10.php
@@ -0,0 +1,23 @@
+<?php
+
+//using cycle <while>
+
+$a = 11;
+
+while($a <= 33)
+{
+    echo "$a <br/>";
+    
+	$a++;
+}
+
+echo '---------------<br/>';
+
+//using cycle <for>
+
+for($i = 11; $i <= 33; $i++)
+{
+    echo "$i <br/>";
+}
+
+
