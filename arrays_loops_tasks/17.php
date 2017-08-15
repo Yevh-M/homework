8792c1871825dd4c5adc587d2891104563346c98
Yevh-M <mynike37@gmail.com>
2017-08-15 22:38:28 +0300

    Output an array of months and dynamic determination of the current month

--- arrays_loops_tasks/17.php
+++ arrays_loops_tasks/17.php
@@ -1,4 +1,25 @@
 <?php
+
+//Output an array of months and dynamic determination of the current month
+
+$months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+$d_month = getdate();
+$month = $d_month["month"];
+$str = "";
+
+foreach($months as $key => $m){
+    if($m == $month){
+        $str .= '<b>' . $m . '</b>' . ', ';
+    }else {
+        $str .= $m . ', ';
+    }
+}
+echo $str;
+?>
+
+
+<?php
+
 //Output only using <foreach> and ternary operator
 
 $months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november","december"];
