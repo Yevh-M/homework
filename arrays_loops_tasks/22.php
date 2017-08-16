be97473a3a2aec3747a082815e9fa8850345380d
Yevh-M <mynike37@gmail.com>
2017-08-16 21:09:41 +0300

    Revert "Revert "<?php""
    This reverts commit 404fd4704aa52509a16b4916f8af974c9cad8190.

---
+++ arrays_loops_tasks/22.php
@@ -0,0 +1,17 @@
+<?php
+//22
+//Output pyramid from the values of two elements
+
+$n = 1;
+
+while($n <= 5){
+    $m = 1;
+    while($n >= $m){
+        echo 'xx';
+        $m++;
+    }
+    echo '</br>';
+    $n++;
+}
+
+
