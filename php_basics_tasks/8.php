55ea4e6cf956d287f86073f55c0f06b26f0737bd
Yevh-M <mynike37@gmail.com>
2017-08-19 15:53:33 +0300

    Task 1 to 8 (corrected verification procedure for variable type)

--- php_basics_tasks/8.php
+++ php_basics_tasks/8.php
@@ -1,20 +1,23 @@
 <?php
+// 8
 
 $name = 'Yevhenii';
-$age = 26;
+$age = '26test';
 
-echo "My name is:{$name}";
+echo "My name is:{$name} </br>";
 
-echo "I am {$age} years old";
+if (!intval($age) || $age < 1 || !preg_match('/^\+?\d+$/', $age)){
+    echo 'Unknown age';
+}else{
+    echo "I am {$age} years old </br>";
 
-
-if ($age >= 18 && $age <= 59) {
-    echo 'You still have to work and work';
-}   if ($age > 59){
+    if ($age >= 18 && $age <= 59) {
+        echo 'You still have to work and work';
+    }
+    if ($age > 59){
         echo 'It\'s time for you to retire';
-}       if ($age >0 && $age <=17){
-            echo 'It\'s too early to work';
-}           if (!intval($age) || $age < 1 || !preg_match('/^\+?\d+$/', $age)){
-                echo 'Unknown age';
+    }
+    if ($age >0 && $age <=17){
+        echo 'It\'s too early to work';
+    }
 }
-
