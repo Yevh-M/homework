c96e4eb8db7f4ad4881add210eddf6991712c12c
Yevh-M <mynike37@gmail.com>
2017-09-01 16:50:46 +0300

    Max and Min value of array of numbers, change places.

--- arrays_loops_tasks/25.php
+++ arrays_loops_tasks/25.php
@@ -3,17 +3,26 @@
 $arr_val = array();
 
 for($count = 1; $count <= 10; $count++){
-    $arr_val[$count] = rand();
+    $arr_val[] = rand(1, 10);
 }
 echo '<pre>';
 print_r ($arr_val);
 echo '<pre/>';
-$arr_max = max ($arr_val);
-$arr_min = min ($arr_val);
+$arr_min = min ($arr_val);//1
+$arr_max = max ($arr_val);//10
 
-$arr_max=$arr_min+$arr_max;
-$arr_min=$arr_max-$arr_min;
-$arr_max=$arr_max-$arr_min;
+foreach($arr_val as $k => $n){
+    if ($n == $arr_min){
+        $arr_val[$k] = $arr_max;
+    }if ($n == $arr_max){
+        $arr_val[$k] = $arr_min;
+    }
+}
+echo '<pre>';
+print_r ($arr_val);
+echo '<pre/>';
 
 echo 'Max value: ' . $arr_max . '</br>';
 echo 'Min value: ' . $arr_min;
+
+
